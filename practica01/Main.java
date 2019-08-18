47b4edc53dd7c55f9622ca0f46d3f3ba3ecdd553
ShadoRoca <53197430+ShadoRoca@users.noreply.github.com>
2019-08-18 11:07:27 -0500

    Add files via upload
    Hice nuevamente practica01

---
+++ practica01/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[]args){
+	System.out.println("openjdk 11.0.2 2019-01-15");
+	System.out.println("OpenJDK Runtime Environment 18.9 (build 11.0.2+9)");
+	System.out.println("OpenJDK 64-Bit Server VM 18.9 (build 11.0.2+9, mixed mode)\n");
+
+	System.out.println("Linux rodrigo-X510URR 5.0.0-25-generic #26~18.04.1-Ubuntu SMP Thu Aug 1 13:51:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux");
+	    }
+}
+
+
+	    
+
+	
