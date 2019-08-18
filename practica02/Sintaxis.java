74300286f237ad558267b80e5d2d792f5a236149
ShadoRoca <53197430+ShadoRoca@users.noreply.github.com>
2019-08-18 15:16:36 -0500

    Corregí Sintaxis.java y cree archivo soluciones

---
+++ practica02/Sintaxis.java
@@ -0,0 +1,27 @@
+import static java.lang.Math.sqrt;
+
+public class Sintaxis {
+
+	public static void main(String[] args) {
+		int a = 3, b = 20, c = -7 ;
+
+		byte prop = 0;
+		short ochoBits = 128;
+		int dieciseisBits = -32769;
+
+		double discriminante = b * b - 4 * a * c;
+		double x1 = (-b + sqrt(discriminante)) / (2 * a); 
+		double x2 = (-b - sqrt(discriminante)) / (2 * a);
+
+		char ch1, ch2;
+		ch1 = 'g'; ch2 = 'w';
+		int d = 1 / (a - 2);
+
+		// Declaraciones
+
+		int var_1 = 214748364;
+		char quote = '\'';
+		
+	}
+
+}
